e28df6ec905890c711864e8b5944894719044beb
unknown <belenizetta@gmail.com>
2022-01-28 16:33:50 -0300

    seeder funcionando

--- backend/seeder.js
+++ backend/seeder.js
@@ -30,3 +30,22 @@ const importData = async () => {
         process.exit(1);
     }
 };
+
+const destroyData = async () => {
+    try{
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+        console.log('Data destroyed!');
+        process.exit();
+    }catch (error){
+        console.error(`${error}`,red.inverse);
+        process.exit(1);
+    }
+};
+
+if (process.argv[2] === 'd'){
+    destroyData();
+}else {
+    importData();
+}
